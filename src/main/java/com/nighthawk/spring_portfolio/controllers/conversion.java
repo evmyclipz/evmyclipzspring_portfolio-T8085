498692c541d6b7c46f5a15b30182f5b0518bafa0
mrohangithub <mrohan2005@gmail.com>
2022-09-12 17:50:58 -0700

    website changes

---
+++ src/main/java/com/nighthawk/spring_portfolio/controllers/conversion.java
@@ -0,0 +1,19 @@
+package com.nighthawk.spring_portfolio.controllers;
+/* MVC code that shows defining a simple Model, calling View, and this file serving as Controller
+ * Web Content with Spring MVCSpring Example: https://spring.io/guides/gs/serving-web-con
+ */
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@ Controller
+public class conversion {
+
+    @GetMapping("/Conversion")
+    public String conversion{
+        
+    }
+    
+}
