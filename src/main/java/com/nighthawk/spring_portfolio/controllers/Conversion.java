f7e24f5581682cad3f448f729835888761381f5c
mrohangithub <mrohan2005@gmail.com>
2022-09-14 17:03:28 -0700

    yea1

--- src/main/java/com/nighthawk/spring_portfolio/controllers/Conversion.java
+++ src/main/java/com/nighthawk/spring_portfolio/controllers/Conversion.java
@@ -12,57 +12,14 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class Conversion {
 
 
-    /*private String reverseString(String out) {
-        char[] ch = out.toCharArray();
-        int x = out.length()-1;
-        String s="";
-        for(;x>=0;x--) {
-            s+=ch[x];
-        }
-        return s;
-    }*/
 
     @GetMapping("/conversion")
     public String number(@RequestParam(name="name") String name, Model model) {
-        /*int x = Integer.parseInt(name);
-        String out="";
-        int a; //
-        for(;x>=1; x=x/16) { //decimal to hexadecimal
-            a = x % 16;
-            if(a>9) {
-                if(a==10) { 
-                    out+= 'A';//appends to string
-                }
-                else if(a == 11) {
-                	out+= 'B';
-                }
-                else if(a == 12) {
-                	out+= 'C';
-                }
-                else if(a == 13) {
-                	out+= 'D';
-                }
-                else if(a == 14) {
-                	out+= 'E';
-                }
-                else if(a == 15) {
-                	out+= 'F';
-                }
-            }
-            else
-            	out+=String.valueOf(a);//appends to string
-            
-        }
-        this.reverseString(out);*/
+        model.addAttribute("name", name);
         name = "Hello World";
-        model.
-
 
         return "conversion";
         
-    
-    
-
     }
 
 }
