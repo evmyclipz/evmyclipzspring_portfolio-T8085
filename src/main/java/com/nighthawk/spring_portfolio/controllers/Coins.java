e962cc6e401daf9959024812dcf5c6021757bd0a
evmyclipz <mrohan2005@gmail.com>
2022-10-02 14:25:48 -0700

    cars

--- src/main/java/com/nighthawk/spring_portfolio/controllers/Coins.java
+++ src/main/java/com/nighthawk/spring_portfolio/controllers/Coins.java
@@ -1,4 +1,4 @@
-package com.evmyclipz.spring_portfolio.mvc.coins;
+package com.nighthawk.spring_portfolio.controllers;
 
 import java.net.URI;
 import java.net.http.HttpClient;
@@ -13,7 +13,51 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
+@RequestMapping("/api/coins")
 public class Coins {
-    
+    private JSONObject body; //last run result
+    private HttpStatus status; //last run status
+    String last_run = null; //last run day of month
+
+    @GetMapping("/coins")
+    public ResponseEntity<JSONObject> getCoins() {
+
+        //calls API once a day, sets body and status properties
+        String today = new Date().toString().substring(0,10); 
+        if (last_run == null || !today.equals(last_run))
+        {
+            try {  //APIs can fail (ie Internet or Service down)
+                
+                //RapidAPI header
+                HttpRequest request = HttpRequest.newBuilder()
+		            .uri(URI.create("https://car-data.p.rapidapi.com/cars?limit=10&page=0"))
+		            .header("X-RapidAPI-Key", "3bef89a477msh7eecb5e7498cd51p1095f2jsn9b36f511d7ec")
+		            .header("X-RapidAPI-Host", "car-data.p.rapidapi.com")
+		            .method("GET", HttpRequest.BodyPublishers.noBody())
+		            .build();
+                
+                HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
+
+                //JSONParser extracts text body and parses to JSONObject
+                this.body = (JSONObject) new JSONParser().parse(response.body());
+                System.out.println(this.body);
+                this.status = HttpStatus.OK;  //200 success
+                this.last_run = today;
+            }
+            catch (Exception e) {  //capture failure info
+                HashMap<String, String> status = new HashMap<>();
+                status.put("status", "RapidApi failure: " + e);
+
+                //Setup object for error
+                this.body = (JSONObject) status;
+                this.status = HttpStatus.INTERNAL_SERVER_ERROR; //500 error
+                this.last_run = null;
+            }
+        }
+
+        //return JSONObject in RESTful style
+        return new ResponseEntity<>(body, status);
+    }
 }
+
