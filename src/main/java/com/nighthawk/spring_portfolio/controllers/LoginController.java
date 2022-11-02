5e30b309893713929a0db85056d079a8b7b6561f
Aryan <emailaryantoday@gmail.com>
2022-11-01 20:42:18 -0700

    shoot

--- src/main/java/com/nighthawk/spring_portfolio/controllers/LoginController.java
+++ src/main/java/com/nighthawk/spring_portfolio/controllers/LoginController.java
@@ -1,17 +1,29 @@
 package com.nighthawk.spring_portfolio.controllers;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+import org.springframsework.web.bind.annotation.RequestMethod;
 
 @Controller
+
 public class LoginController {
 
     @RequestMapping("/login")
-public String showForm(){
+    public String showForm() {
 
-return "login";
+        return "login";
 
-}
+    }
+
+    @RequestMapping(path = "/register", method = RequestMethod.POST)
+    public String handleForm(HttpServletRequest request) {
+
+        String email = request.getParameter("fname");
+        System.out.println(email);
+        return "";
+
+    }
 
 }
