af55a2d628088641dc200f1be72471ec23fa1c29
mrohangithub <mrohan2005@gmail.com>
2022-11-02 21:54:59 -0700

    new changes

---
+++ src/main/java/com/nighthawk/spring_portfolio/controllers/UserService.java
@@ -0,0 +1,34 @@
+package com.nighthawk.spring_portfolio.controllers;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.nighthawk.spring_portfolio.controllers.UserInfoRespository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Component
+public class UserService {
+
+    private final UserInfoRespository userRepo;
+
+    @Autowired
+    public UserService(UserInfoRespository ur)
+    {
+        userRepo = ur;
+    }
+
+    public boolean userExists(String name)
+    {
+        Optional<UserInfo> exists = userRepo.findUserInfoByName(name);
+        return exists.isPresent();
+    }
+
+    public UserInfo saveUser(UserInfo user)
+    {
+        return userRepo.save(user);
+    }
+    
+}
