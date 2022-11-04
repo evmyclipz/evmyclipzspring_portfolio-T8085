d232b5b10e4b0fae0b606d34578923521cfaea4d
Ritvik-Keerthi <89219486+Ritvik-Keerthi@users.noreply.github.com>
2022-11-04 09:13:14 -0700

    Create FoodTracker.java

---
+++ src/main/java/com/nighthawk/spring_portfolio/controllers/FoodTracker.java
@@ -0,0 +1,27 @@
+package com.nighthawk.spring_portfolio.controllers;
+
+import org.springframework.context.annotation.Configuration;
+
+import com.nighthawk.spring_portfolio.controllers.ExerciseRepository;
+import com.nighthawk.spring_portfolio.controllers.Exercise;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import java.util.*;
+
+@Configuration //
+public class FoodTracker {
+  
+  Arraylist<String> list = new ArrayList<String>();
+  list.add('Avocado');
+  list.add('Milk');
+  list.add('Toast');
+  list.add('Pork');
+  list.add('Beef');
+  list.add('Rice alone');
+  list.add('Chicken and Rice');
+  list.add('Banana');
+  list.add('Sandwich');
+  list.add('Apple');
+  list.add('Eggs');
+  
+}
