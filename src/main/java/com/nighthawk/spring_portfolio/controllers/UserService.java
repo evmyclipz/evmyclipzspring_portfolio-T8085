8ef0d845167fbaa26a20c0ba2dcf106e0aae2b8f
mrohangithub <mrohan2005@gmail.com>
2022-11-03 17:29:14 -0700

    html validate

--- src/main/java/com/nighthawk/spring_portfolio/controllers/UserService.java
+++ src/main/java/com/nighthawk/spring_portfolio/controllers/UserService.java
@@ -3,7 +3,7 @@ package com.nighthawk.spring_portfolio.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-
+import com.nighthawk.spring_portfolio.controllers.ExerciseRepository; 
 import com.nighthawk.spring_portfolio.controllers.UserInfoRespository;
 
 import java.util.List;
@@ -13,11 +13,13 @@ import java.util.Optional;
 public class UserService {
 
     private final UserInfoRespository userRepo;
+    private final ExerciseRepository repository;
 
     @Autowired
-    public UserService(UserInfoRespository ur)
+    public UserService(UserInfoRespository ur,ExerciseRepository repo)
     {
         userRepo = ur;
+        repository = repo;
     }
 
     public boolean userExists(String name)
@@ -29,6 +31,11 @@ public class UserService {
     public UserInfo saveUser(UserInfo user)
     {
         return userRepo.save(user);
+    }
+
+    public List<Exercise> getExercises()
+    {
+        return repository.findAll(); //retreieves Excercise data from sqlite database
     }
 
     public List<UserInfo> getUsers()
